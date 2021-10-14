fc6c29e64160d3b7ba86fc0c111c61c73126d9f9
ASJADSYED08 <hr49335@gmail.com>
2021-10-14 23:26:16 +0500

    first commit

---
+++ code.py
@@ -0,0 +1,62 @@
+""" Arithmetic_Operators
+ Assignment_Operator
+ Comparision_Operators
+ Logical_Operators
+ Identity_Operators
+ Membership_Operators
+ Bitwise_Operators"""
+
+#Arithmetic_Operators
+print("Arithmetic_Operators")
+print("4*2=",4*2)
+print("4+2=",4+2)
+print("4-2=",4-2)
+print("4/2=",4/2)
+print("4**2=",4**2)
+print("4//2=",4//2)
+print("4&2=",4&2)
+
+#Assignment_Operator
+print("Assignment_Operator")
+x=5
+print(x)
+x+=7
+print(x)
+x*=2
+print(x)
+
+#Comparision_Operators
+print("Comparision_Operators")
+x=5
+print(x==6)
+print(x!=7)
+print(x>4)
+print(x<5)
+
+#Logical_Operators
+print("Logical_Operators")
+a=True
+b=False
+print(a or b)
+print(a and b)
+
+#Identity_Operators
+print("Identity_Operators")
+a=5
+b=2
+print(a is b)
+print(a is not b)
+print(4 is 3)
+print(4 is 4)
+
+#Membership_Operators
+print("Membership_Operators")
+list=[22,12,4,5,45,11,10,9]
+print(6 in list)
+print(6 not in list)
+
+# Bitwise_Operators
+print("Bitwise_Operators")
+print(0 | 1)
+print(0 & 1)
+
